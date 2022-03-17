afb39d9bc509e96faadd6a8e7698dd717f64768a
LittleBeannie <yujie.zhao@merck.com>
2022-03-17 15:09:06 -0400

    add info_scale in `gs_power_npe`

--- R/gs_power_npe.r
+++ R/gs_power_npe.r
@@ -39,6 +39,9 @@ NULL
 #' impacts null hypothesis bound calculation
 #' @param info1 statistical information under hypothesis used for futility bound calculation if different from
 #' \code{info}; impacts futility hypothesis bound calculation
+#' @param info_scale \code{2} (default): do calculations as currently specified. Since this is default, all will be backwards compatible.
+#' \code{0}: use null hypothesis info0 for all calculations
+#' \code{1}: use alternate hypothesis info1 for all calculations
 #' @param binding indicator of whether futility bound is binding; default of FALSE is recommended
 #' @param upper function to compute upper bound
 #' @param lower function to compare lower bound
@@ -88,7 +91,7 @@ NULL
 #' library(dplyr)
 #' 
 #' # Default (single analysis; Type I error controlled)
-#' gs_power_npe2(theta = 0) %>% 
+#' gs_power_npe(theta = 0) %>% 
 #'   filter(Bound == "Upper")
 #' 
 #' # Fixed bound
@@ -158,6 +161,7 @@ NULL
 gs_power_npe <- function(
   theta = .1, theta1 = NULL,
   info = 1, info1 = NULL, info0 = NULL,
+  info_scale = 2,
   binding = FALSE,
   upper = gs_b, upar = qnorm(.975), test_upper = TRUE,
   lower = gs_b, lpar = -Inf, test_lower = TRUE,
@@ -169,8 +173,10 @@ gs_power_npe <- function(
   if (is.null(info0)) info0 <- info
   if (is.null(info1)) info1 <- info
   if (length(info1) != length(info) || length(info0) != length(info)) stop("gs_power_npe: length of info, info0, info1 must be the same")
+  
   if (length(theta) == 1 && K > 1) theta <- rep(theta, K)
   if (is.null(theta1)){theta1 <- theta}else if (length(theta1)==1) theta1 <- rep(theta1, K)
+  
   if (length(test_upper) == 1 && K > 1) test_upper <- rep(test_upper, K)
   if (length(test_lower) == 1 && K > 1) test_lower <- rep(test_lower, K)
   
@@ -184,6 +190,7 @@ gs_power_npe <- function(
   hgm1 <- NULL
   upperProb <- rep(NA, K)
   lowerProb <- rep(NA, K)
+  info_used <- switch (info_scale + 1, info0, info1, info, "gs_power_npe: please input info_scale in c(0, 1, 2)!")
   
   # --------------------------------------------- #
   #     calculate crossing probability            #
@@ -201,8 +208,8 @@ gs_power_npe <- function(
     
     if(k == 1){
       # compute the probability to cross upper/lower bound
-      upperProb[1] <- if(b[1] < Inf) {pnorm(b[1], mean = sqrt(info[1]) * theta[1], lower.tail = FALSE)}else{0}
-      lowerProb[1] <- if(a[1] > -Inf){pnorm(a[1], mean = sqrt(info[1]) * theta[1])}else{0}
+      upperProb[1] <- if(b[1] < Inf) {pnorm(b[1], mean = sqrt(info_used[1]) * theta[1], lower.tail = FALSE)}else{0}
+      lowerProb[1] <- if(a[1] > -Inf){pnorm(a[1], mean = sqrt(info_used[1]) * theta[1])}else{0}
       # update the grids
       hgm1_0 <- h1(r = r, theta = 0,         I = info0[1], a = if(binding){a[1]}else{-Inf}, b = b[1])
       hgm1_1 <- h1(r = r, theta = theta1[1], I = info1[1], a = a[1], b = b[1])
@@ -210,13 +217,13 @@ gs_power_npe <- function(
     }else{
       # compute the probability to cross upper bound
       upperProb[k] <- if(b[k]< Inf){
-        hupdate(r = r, theta = theta[k], I = info[k], a = b[k], b = Inf,
-                thetam1 = theta[k - 1], Im1 = info[k - 1], gm1 = hgm1) %>% summarise(sum(h)) %>% as.numeric()
+        hupdate(r = r, theta = theta[k], I = info_used[k], a = b[k], b = Inf,
+                thetam1 = theta[k - 1], Im1 = info_used[k - 1], gm1 = hgm1) %>% summarise(sum(h)) %>% as.numeric()
       }else{0}
       # compute the probability to cross lower bound
       lowerProb[k] <- if(a[k] > -Inf){
-        hupdate(r = r, theta = theta[k], I = info[k], a = -Inf, b = a[k],
-                thetam1 = theta[k - 1], Im1 = info[k - 1], gm1 = hgm1) %>% summarise(sum(h)) %>% as.numeric()
+        hupdate(r = r, theta = theta[k], I = info_used[k], a = -Inf, b = a[k],
+                thetam1 = theta[k - 1], Im1 = info_used[k - 1], gm1 = hgm1) %>% summarise(sum(h)) %>% as.numeric()
       }else{0}
       
       # update the grids
@@ -235,77 +242,43 @@ gs_power_npe <- function(
     Analysis = rep(1:K, 2),
     Bound = c(rep("Upper", K), rep("Lower", K)),
     Z = c(b, a),
-    Probability = c(cumsum(upperProb),
-                    cumsum(lowerProb)),
+    Probability = c(cumsum(upperProb), cumsum(lowerProb)),
     theta = rep(theta, 2),
     theta1 = rep(theta1, 2),
-    IF = rep(info0 / max(info0), 2),
+    IF = rep(info_used / max(info_used), 2),
     info = rep(info, 2),
     info0 = rep(info0, 2),
     info1 = rep(info1, 2),
-    hypothesis = rep("H1", 2*K))
+    hypothesis = rep("H1", 2*K)) %>% filter(abs(Z) < Inf)
   
   # --------------------------------------------- #
   #     calculate crossing probability            #
   #       under the null hypothesis               #
   #            i.e., theta == 0                   #
   # --------------------------------------------- #
-  # COMPUTE BOUNDS
-  # y0 <- gsdmvn::gs_power_npe(
-  # theta = rep(0, nrow(analyses)),
-  # info = analyses$info0,
-  # info0 = analyses$info0,
-  # Just specify Z-values for bounds
-  # upper  = gsdmvn::gs_b,
-  # lower = gsdmvn::gs_b,
-  # Note that we must have an upper and a lower bound for each analysis
-  # (any bound not to be used will be infinite)
-  # upar = (bounds %>% filter(Bound == "Upper"))$Z,
-  # lpar = (bounds %>% filter(Bound == "Lower"))$Z
-  # ) %>% filter(abs(Z) < Inf)
   for(k in 1:K){
-    # calculate/update the lower bound
-    a[k] <- gsdmvn::gs_b(k = k, par = a #, par = lpar,
-                         # info = info1, r = r, tol = tol, test_bound = test_lower, hgm1 = hgm1_1
-                         # theta = rep(0, K), efficacy = FALSE)
-    )
-    # calculate/update the upper bound
-    b[k] <- gsdmvn::gs_b(k = k, par = b #, par = upar,
-                         #info = info0, r = r, tol = tol, test_bound = test_upper, hgm1 = hgm1_0,  
-    )
+    # calculate/update the lower/upper bound
+    a[k] <- gsdmvn::gs_b(k = k, par = a)
+    b[k] <- gsdmvn::gs_b(k = k, par = b)
     if(k == 1){
-      # upperProb[1] <- if(b[1] < Inf) {pnorm(b[1], mean = sqrt(info[1]) * theta[1], lower.tail = FALSE)}else{0}
-      # lowerProb[1] <- if(a[1] > -Inf){pnorm(a[1], mean = sqrt(info[1]) * theta[1])}else{0}
       upperProb[1] <- if(b[1] < Inf) {pnorm(b[1], mean = 0, lower.tail = FALSE)}else{0}
       lowerProb[1] <- if(a[1] > -Inf){pnorm(a[1], mean = 0)}else{0}
-      # hgm1_1 <- h1(r = r, theta = theta1[1], I = info1[1], a = a[1], b = b[1])
-      # hgm1   <- h1(r = r, theta = theta[1],  I = info[1],  a = a[1], b = b[1])
-      hgm1   <- h1(r = r, theta = 0, I = info[1],  a = a[1], b = b[1])
+      hgm1 <- h1(r = r, theta = 0, I = info_used[1],  a = a[1], b = b[1])
     }else{
       # calculate the probability to cross upper bound
       upperProb[k] <- if(b[k] < Inf){
-        # hupdate(r = r, theta = theta[k], I = info[k], a = b[k], b = Inf,
-        #         thetam1 = theta[k - 1], Im1 = info[k - 1], gm1 = hgm1) %>% summarise(sum(h)) %>% as.numeric()
-        hupdate(r = r, theta = 0, I = info[k], a = b[k], b = Inf,
-                thetam1 = 0, Im1 = info[k - 1], gm1 = hgm1) %>% summarise(sum(h)) %>% as.numeric()
+        hupdate(r = r, theta = 0, I = info_used[k], a = b[k], b = Inf,
+                thetam1 = 0, Im1 = info_used[k - 1], gm1 = hgm1) %>% summarise(sum(h)) %>% as.numeric()
       }else{0}
       # calculate the probability to cross lower bound
       lowerProb[k] <- if(a[k] > -Inf){
-        # hupdate(r = r, theta = theta[k], I = info[k], a = -Inf, b = a[k],
-        #         thetam1 = theta[k - 1], Im1 = info[k - 1], gm1 = hgm1) %>% summarise(sum(h)) %>% as.numeric()
-        hupdate(r = r, theta = 0, I = info[k], a = -Inf, b = a[k],
-                thetam1 = 0, Im1 = info[k - 1], gm1 = hgm1) %>% summarise(sum(h)) %>% as.numeric()
+        hupdate(r = r, theta = 0, I = info_used[k], a = -Inf, b = a[k],
+                thetam1 = 0, Im1 = info_used[k - 1], gm1 = hgm1) %>% summarise(sum(h)) %>% as.numeric()
       }else{0}
       
       if(k < K){
-        # hgm1_0 <- hupdate(r = r, theta = 0, thetam1 = 0, I = info0[k], Im1 = info0[k-1],
-        #                   a = if(binding){a[k]}else{-Inf}, b = b[k], gm1 = hgm1_0)
-        # hgm1_1 <- hupdate(r = r, theta = theta1[k], thetam1 = theta1[k-1],I = info1[k], Im1 = info1[k-1],
-        #                   a = a[k], b = b[k], gm1 = hgm1_1)
-        # hgm1   <- hupdate(r = r, theta = theta[k], thetam1 = theta[k-1], I = info[k],  Im1 = info[k-1],
-        #                   a = a[k], b = b[k], gm1 = hgm1)
-        hgm1   <- hupdate(r = r, theta = 0, thetam1 = 0, I = info[k], Im1 = info[k-1],  
-                          a = a[k], b = b[k], gm1 = hgm1)
+        hgm1 <- hupdate(r = r, theta = 0, thetam1 = 0, I = info_used[k], Im1 = info_used[k-1],  
+                        a = a[k], b = b[k], gm1 = hgm1)
       }
     }
   }
@@ -314,15 +287,14 @@ gs_power_npe <- function(
     Analysis = rep(1:K, 2),
     Bound = c(rep("Upper", K), rep("Lower", K)),
     Z = c(b, a),
-    Probability = c(cumsum(upperProb),
-                    cumsum(lowerProb)),
+    Probability = c(cumsum(upperProb), cumsum(lowerProb)),
     theta = rep(rep(0, K), 2),
     theta1 = rep(rep(0, K), 2),
-    IF = rep(info1 / max(info1), 2),
-    info = rep(info0, 2),  #rep(info, 2),
+    IF = rep(info_used / max(info_used), 2),
+    info = rep(info, 2),  
     info0 = rep(info0, 2),
     info1 = rep(info1, 2),
-    hypothesis = rep("H0", 2*K))%>% filter(abs(Z) < Inf)
+    hypothesis = rep("H0", 2*K)) %>% filter(abs(Z) < Inf)
   
   out <- union_all(
     table_H1,
