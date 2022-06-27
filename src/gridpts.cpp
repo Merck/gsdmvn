123a3e3e3d000ac193136e42193b93a68230e335
Jianxiao Yang <yangjx@g.ucla.edu>
2022-06-27 16:54:07 -0700

    fix a typo in the last commit and preallocate x for the for loop

--- src/gridpts.cpp
+++ src/gridpts.cpp
@@ -130,9 +130,10 @@ List hupdateRcpp(int r, double theta, double I, double a, double b,
   // update integration
   double mu = theta * I - thetam1 * Im1;
   NumericVector h(z.size());
+  NumericVector x(zm1.size());
   for(int i = 0; i < z.size(); i++){
-    NumericVector x = dnorm((z[i] * rtI - zm1 * rtIm1 - mu) / rtdelta);
-    h[i] = sum(hm1 * dnorm(x));
+    x = dnorm((z[i] * rtI - zm1 * rtIm1 - mu) / rtdelta);
+    // h[i] = sum(hm1 * dnorm(x));
     h[i] = std::inner_product(hm1.begin(), hm1.end(), x.begin(), 0.0);
   }
   h = h * w * rtI / rtdelta;
