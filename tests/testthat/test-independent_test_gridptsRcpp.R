e035a9708cbc722ac5073f5308e1569593dda346
Jianxiao Yang <yangjx@g.ucla.edu>
2022-07-06 23:31:44 -0700

    add independent tests

---
+++ tests/testthat/test-independent_test_gridptsRcpp.R
@@ -0,0 +1,14 @@
+
+testthat::test_that("compare gridptsRcpp results with gsDesign::normalGrid results", {
+  test1 <- gsdmvn::gridptsRcpp(r = 18, mu = 4, a = -Inf, b = Inf)
+  x <- gsDesign::normalGrid(r = 18, bounds = c(-40, 40), mu = 4, sigma = 1)
+  expect_equal(test1$w, x$gridwgts)
+  expect_equal(test1$z, x$z)
+
+  test2 <- gsdmvn::gridptsRcpp(r = 18, mu = 2, a = -Inf, b = Inf)
+  y <- gsDesign::normalGrid(r = 18, bounds = c(-40, 40), mu = 2, sigma = 1)
+  expect_equal(test2$w,y$gridwgts)
+  expect_equal(test2$z,y$z)
+})
+
+
